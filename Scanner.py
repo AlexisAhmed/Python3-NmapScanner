ba08b7345dbe35ccacc007e5a7d606d35d446861
Giacomo <giacomo.venturini2@studio.unibo.it>
2019-01-22 10:12:25 +0100

    remove code duplication add comments explaining nmap flags

--- Scanner.py
+++ Scanner.py
@@ -1,50 +1,53 @@
 #!/usr/bin/python3
 
+# run with sudo Python3 Scanner.py
+
+# pip3 install python-nmap
 import nmap
 
 scanner = nmap.PortScanner()
 
 print("Welcome, this is a simple nmap automation tool")
-print("<----------------------------------------------------->")
+print("<-------------------------------------------------------->")
+print("Currently installed nmap version: ", scanner.nmap_version())
+print("<-------------------------------------------------------->")
 
-ip_addr = input("Please enter the IP address you want to scan: ")
-print("The IP you entered is: ", ip_addr)
+ip_addr = '127.0.0.1'  # default ip address
+prompt_str = "Please enter the IP address you want to scan ["+ip_addr+"]: "
+ip_addr = input(prompt_str) or ip_addr
+print("Selected IP: ", ip_addr)
 type(ip_addr)
 
-resp = input("""\nPlease enter the type of scan you want to run
-                1)SYN ACK Scan
-                2)UDP Scan
-                3)Comprehensive Scan \n""")
+resp = input("""\nEnter the scan type you want to run
+                1) SYN ACK Scan
+                2) UDP Scan
+                3) Comprehensive Scan \n""")
 print("You have selected option: ", resp)
 
+
+def run_scan(args: str, port_range: str = '1-1024'):
+    scanner.scan(ip_addr, port_range, args)
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    protocols = scanner[ip_addr].all_protocols()
+    print(protocols)
+    for protocol in protocols:
+        print("["+protocol+"] open ports: ", scanner[ip_addr][protocol].keys())
+
+
+# -v verbose
 if resp == '1':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, '1-1024', '-v -sS')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+    # -sS TCP SYN scan
+    run_scan('-v -sS')
 elif resp == '2':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, '1-1024', '-v -sU')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['udp'].keys())
+    # -sU UDP SYN scan
+    run_scan('-v -sU')
 elif resp == '3':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
-elif resp >= '4':
+    # -sS TCP SYN scan
+    # -sV Version detection
+    # -sC script scan using the default set of scripts
+    # -A Aggressive scan options
+    # -O Enable OS detection
+    run_scan('-v -sS -sV -sC -A -O')
+else:
     print("Please enter a valid option")
-
-
-
-
-
-
-
-
