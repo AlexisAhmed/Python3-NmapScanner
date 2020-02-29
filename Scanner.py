b7455a50b80207ff1fe64ebb80099dcc428ffad2
samsep0il <61392151+samsep0il@users.noreply.github.com>
2020-02-29 22:14:03 +0000

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -1,50 +1,12 @@
 #!/usr/bin/python3
 
-import nmap
+import socket 
 
-scanner = nmap.PortScanner()
+portas = [21, 22, 80, 8080, 443]
 
-print("Welcome, this is a simple nmap automation tool")
-print("<----------------------------------------------------->")
-
-ip_addr = input("Please enter the IP address you want to scan: ")
-print("The IP you entered is: ", ip_addr)
-type(ip_addr)
-
-resp = input("""\nPlease enter the type of scan you want to run
-                1)SYN ACK Scan
-                2)UDP Scan
-                3)Comprehensive Scan \n""")
-print("You have selected option: ", resp)
-
-if resp == '1':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, '1-1024', '-v -sS')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
-elif resp == '2':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, '1-1024', '-v -sU')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['udp'].keys())
-elif resp == '3':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
-elif resp >= '4':
-    print("Please enter a valid option")
-
-
-
-
-
-
-
-
+for porta in portas:
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientesettimeout(1.0)
+    codigo = cliente.connect_ex(("IP", porta))
+    if codigo == 0:
+        print(porta, "OPEN")
