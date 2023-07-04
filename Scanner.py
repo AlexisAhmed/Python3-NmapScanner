072e07ac149d4775e36046ee312b9de7b99a99f6
numaan911098 <50366430+numaan911098@users.noreply.github.com>
2023-07-04 12:27:49 +0530

    Update Scanner.py
    There were a lot of typos for example  print("nmap version: "sccaner.nmap_version()). It should be scanner.nmap_version() and so many i didn't changed add any new code or function to it i just fixed the errors so that it can run.

--- Scanner.py
+++ Scanner.py
@@ -9,27 +9,24 @@ print("<----------------------------------------------------->")
 
 ip_addr = input("Please enter the IP address you want to scan: ")
 print("The IP you entered is: ", ip_addr)
-type(ip_addr)
 
 resp = input("""\nPlease enter the type of scan you want to run
-                1)SYN ACK Scan
-                2)UDP Scan
-                3)Comprehensive Scan \n""")
+                1) SYN ACK Scan
+                2) UDP Scan
+                3) Comprehensive Scan \n""")
 print("You have selected option: ", resp)
-resp_dict={'1':['-v -sS','tcp'],'2':['-v -sU','udp'],'3':['-v -sS -sV -sC -A -O','tcp']}
+
+resp_dict = {'1': ['-v -sS', 'tcp'], '2': ['-v -sU', 'udp'], '3': ['-v -sS -sV -sC -A -O', 'tcp']}
 if resp not in resp_dict.keys():
-    print("enter a valid option")
+    print("Enter a valid option")
 else:
-    print("nmap version: "sccaner.nmap_version())
-    scanner.scan(ip_addr,"1-1024",resp_dict[resp][0]) #the # are port range to scan, the last part is the scan type
+    print("nmap version:", scanner.nmap_version())
+    scanner.scan(ip_addr, "1-1024", resp_dict[resp][0])
     print(scanner.scaninfo())
-    if scanner.scaninfo()=='up':
-        print("Scanner Status: ",scanner[ip_addr].state())
+    if 'up' in scanner[ip_addr].state():
+        print("Scanner Status: ", scanner[ip_addr].state())
         print(scanner[ip_addr].all_protocols())
-        print("Open Ports: ",scanner[ip_addr][resp_dict[resp][1]].keys())  #display all open ports
-
-
-
+        print("Open Ports: ", scanner[ip_addr][resp_dict[resp][1]].keys())
 
 
 
